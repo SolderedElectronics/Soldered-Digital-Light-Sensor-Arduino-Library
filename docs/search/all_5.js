35fe5a8353c5d0d88b3c197ac58d959f229c6d71
Docs Robot <robot@example.com>
2021-12-17 09:01:56 +0000

    Committing documentation changes

--- docs/search/all_5.js
+++ docs/search/all_5.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['initializenative_0',['initializeNative',['../class_l_t_r507.html#a29346ddc75931b5d427cab31df20337d',1,'LTR507::initializeNative()'],['../class_digital__light__sensor__easyc_1_1_easy_c.html#adcd4a84ff4dad08e2a35ee8a4f840ccf',1,'Digital_light_sensor_easyc::EasyC::initializeNative()']]]
+  ['init_0',['init',['../class_l_t_r507.html#a6a9fa2bf18492febfb5352c7f18a2f52',1,'LTR507']]],
+  ['initializenative_1',['initializeNative',['../class_l_t_r507.html#a29346ddc75931b5d427cab31df20337d',1,'LTR507::initializeNative()'],['../class_digital__light__sensor__easyc_1_1_easy_c.html#adcd4a84ff4dad08e2a35ee8a4f840ccf',1,'Digital_light_sensor_easyc::EasyC::initializeNative()']]]
 ];
