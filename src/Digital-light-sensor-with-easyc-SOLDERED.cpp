9c0d5ef08af0d06596920b19ba3f1250fbb49347
Clang Robot <robot@example.com>
2022-01-18 11:27:09 +0000

    Committing clang-format changes

--- src/Digital-light-sensor-with-easyc-SOLDERED.cpp
+++ src/Digital-light-sensor-with-easyc-SOLDERED.cpp
@@ -16,16 +16,20 @@
  *
  * @param int _pin          Example parameter.
  */
-void LTR507::Sensor(int _pin) {
-  pin = _pin;
-  native = 1;
+void LTR507::Sensor(int _pin)
+{
+    pin = _pin;
+    native = 1;
 }
 
 /**
  * @brief                   Overloaded function for virtual in base class to
  * initialize sensor specific.
  */
-void LTR507::initializeNative() { pinMode(pin, INPUT); }
+void LTR507::initializeNative()
+{
+    pinMode(pin, INPUT);
+}
 
 /**
  * @brief                   Function which gets light intensity data through I2C
@@ -33,19 +37,19 @@ void LTR507::initializeNative() { pinMode(pin, INPUT); }
  *
  * @return                  light intensity
  */
-uint16_t LTR507::getLightIntensity() {
-  readRegister(ALS_PS_STATUS_ADDRESS, raw,
-               sizeof(uint8_t)); // Get ALS and PS state register
-  if (!(0x4 & raw[0])) // If bit 2 is set to 0, Digital Light Sensor measuring
-                       // is already read
-  {
-    return 0;
-  }
-  readRegister(LIGHT_INTENSITY_ADDRESS, raw,
-               sizeof(uint8_t) *
-                   2); // Get two bytes that consist of light intensity data
-  return raw[0] | raw[1] << 8; // I2C sends 8 bits at once, this function merges
-                               // two bytes into one variable
+uint16_t LTR507::getLightIntensity()
+{
+    readRegister(ALS_PS_STATUS_ADDRESS, raw,
+                 sizeof(uint8_t)); // Get ALS and PS state register
+    if (!(0x4 & raw[0]))           // If bit 2 is set to 0, Digital Light Sensor measuring
+                                   // is already read
+    {
+        return 0;
+    }
+    readRegister(LIGHT_INTENSITY_ADDRESS, raw,
+                 sizeof(uint8_t) * 2); // Get two bytes that consist of light intensity data
+    return raw[0] | raw[1] << 8;       // I2C sends 8 bits at once, this function merges
+                                       // two bytes into one variable
 }
 
 /**
@@ -53,10 +57,10 @@ uint16_t LTR507::getLightIntensity() {
  *
  * @return                  1 if device is responding, 0 if not
  */
-bool LTR507::Available() {
-  return !(
-      sendAddress(PART_ID_REG)); // checking if address is succesfully sent, if
-                                 // not device is not ready to communicate
+bool LTR507::Available()
+{
+    return !(sendAddress(PART_ID_REG)); // checking if address is succesfully sent, if
+                                        // not device is not ready to communicate
 }
 
 /**
@@ -64,17 +68,21 @@ bool LTR507::Available() {
  *
  * @param bool _mode         1 for enable, 0 for disable
  */
-void LTR507::setALSMode(bool mode) {
-  uint8_t reg_state[2];
-  char buff;
-  readRegister(ALS_CONTR_REG, &buff, sizeof(char));
-  if (mode) {
-    reg_state[1] = buff | 0x02;
-  } else {
-    reg_state[1] = buff & 0xFD;
-  }
-  reg_state[0] = ALS_CONTR_REG;
-  sendData(reg_state, sizeof(uint8_t) * 2);
+void LTR507::setALSMode(bool mode)
+{
+    uint8_t reg_state[2];
+    char buff;
+    readRegister(ALS_CONTR_REG, &buff, sizeof(char));
+    if (mode)
+    {
+        reg_state[1] = buff | 0x02;
+    }
+    else
+    {
+        reg_state[1] = buff & 0xFD;
+    }
+    reg_state[0] = ALS_CONTR_REG;
+    sendData(reg_state, sizeof(uint8_t) * 2);
 }
 
 /**
@@ -83,17 +91,21 @@ void LTR507::setALSMode(bool mode) {
  *
  * @param bool _mode         1 for enable, 0 for disable
  */
-void LTR507::setPSMode(bool mode) {
-  uint8_t reg_state[2];
-  char buff;
-  readRegister(PS_CONTR_REG, &buff, sizeof(char));
-  if (mode) {
-    reg_state[1] = buff | 0x02;
-  } else {
-    reg_state[1] = buff & 0xFD;
-  }
-  reg_state[0] = PS_CONTR_REG;
-  sendData(reg_state, sizeof(uint8_t) * 2);
+void LTR507::setPSMode(bool mode)
+{
+    uint8_t reg_state[2];
+    char buff;
+    readRegister(PS_CONTR_REG, &buff, sizeof(char));
+    if (mode)
+    {
+        reg_state[1] = buff | 0x02;
+    }
+    else
+    {
+        reg_state[1] = buff & 0xFD;
+    }
+    reg_state[0] = PS_CONTR_REG;
+    sendData(reg_state, sizeof(uint8_t) * 2);
 }
 
 /**
@@ -103,10 +115,11 @@ void LTR507::setPSMode(bool mode) {
  *
  * @return                  specific register data
  */
-char LTR507::getReg(int addr) {
-  char a = 'a';
-  readRegister(addr, &a, 1);
-  return a;
+char LTR507::getReg(int addr)
+{
+    char a = 'a';
+    readRegister(addr, &a, 1);
+    return a;
 }
 
 /**
@@ -114,13 +127,14 @@ char LTR507::getReg(int addr) {
  *
  * @param uint8_t _state          Binary code from macro defines
  */
-void LTR507::setALSGain(uint8_t state) {
-  uint8_t reg_state[2];
-  char buff;
-  readRegister(ALS_CONTR_REG, &buff, sizeof(char));
-  reg_state[1] = (buff & B11110011) | state << 2;
-  reg_state[0] = ALS_CONTR_REG;
-  sendData(reg_state, sizeof(uint8_t) * 2);
+void LTR507::setALSGain(uint8_t state)
+{
+    uint8_t reg_state[2];
+    char buff;
+    readRegister(ALS_CONTR_REG, &buff, sizeof(char));
+    reg_state[1] = (buff & B11110011) | state << 2;
+    reg_state[0] = ALS_CONTR_REG;
+    sendData(reg_state, sizeof(uint8_t) * 2);
 }
 
 /**
@@ -128,13 +142,14 @@ void LTR507::setALSGain(uint8_t state) {
  *
  * @param uint8_t _rate          Binary code from macro defines
  */
-void LTR507::setALSBitWidth(uint8_t rate) {
-  uint8_t reg_state[2];
-  char buff;
-  readRegister(ALS_MEAS_RATE, &buff, sizeof(char));
-  reg_state[1] = (buff & B00011111) | rate << 5;
-  reg_state[0] = ALS_MEAS_RATE;
-  sendData(reg_state, sizeof(uint8_t) * 2);
+void LTR507::setALSBitWidth(uint8_t rate)
+{
+    uint8_t reg_state[2];
+    char buff;
+    readRegister(ALS_MEAS_RATE, &buff, sizeof(char));
+    reg_state[1] = (buff & B00011111) | rate << 5;
+    reg_state[0] = ALS_MEAS_RATE;
+    sendData(reg_state, sizeof(uint8_t) * 2);
 }
 
 /**
@@ -143,24 +158,26 @@ void LTR507::setALSBitWidth(uint8_t rate) {
  *
  * @param uint8_t _rate          Binary code from macro defines
  */
-void LTR507::setALSMeasRate(uint8_t rate) {
-  uint8_t reg_state[2];
-  char buff;
-  readRegister(ALS_MEAS_RATE, &buff, sizeof(char));
-  reg_state[1] = (buff & B11111000) | rate;
-  reg_state[0] = ALS_MEAS_RATE;
-  sendData(reg_state, sizeof(uint8_t) * 2);
+void LTR507::setALSMeasRate(uint8_t rate)
+{
+    uint8_t reg_state[2];
+    char buff;
+    readRegister(ALS_MEAS_RATE, &buff, sizeof(char));
+    reg_state[1] = (buff & B11111000) | rate;
+    reg_state[0] = ALS_MEAS_RATE;
+    sendData(reg_state, sizeof(uint8_t) * 2);
 }
 
 /**
  * @brief                   Function which initializes sensor
  *
  */
-void LTR507::init() {
-  begin(LTR507_ADDRESS);
-  setALSMode(1);
-  setPSMode(1);
-  setALSGain(ALS_GAIN_1);
-  setALSBitWidth(ALS_BIT_WIDTH_4);
-  setALSMeasRate(ALS_MEAS_RATE_2);
+void LTR507::init()
+{
+    begin(LTR507_ADDRESS);
+    setALSMode(1);
+    setPSMode(1);
+    setALSGain(ALS_GAIN_1);
+    setALSBitWidth(ALS_BIT_WIDTH_4);
+    setALSMeasRate(ALS_MEAS_RATE_2);
 }
