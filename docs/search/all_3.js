1465a5493ad9726ebc50e3ecb26cc46ef039e016
Docs Robot <robot@example.com>
2022-01-18 11:17:17 +0000

    Committing documentation changes

--- docs/search/all_3.js
+++ docs/search/all_3.js
@@ -1,7 +1,6 @@
 var searchData=
 [
   ['easyc_0',['EasyC',['../class_s_i114_x__light__sensor__easyc_1_1_easy_c.html#af555ae4d6f7367320947051d89a710bd',1,'SI114X_light_sensor_easyc::EasyC::EasyC()'],['../class_s_i114_x__light__sensor__easyc_1_1_easy_c.html',1,'SI114X_light_sensor_easyc::EasyC']]],
-  ['easyc_2ecpp_1',['easyC.cpp',['../easy_c_8cpp.html',1,'']]],
-  ['easyc_2eh_2',['easyC.h',['../easy_c_8h.html',1,'']]],
-  ['err_3',['err',['../class_s_i114_x__light__sensor__easyc_1_1_easy_c.html#a693daef884a0a77504e298c01cf3a885',1,'SI114X_light_sensor_easyc::EasyC']]]
+  ['easyc_2eh_1',['easyC.h',['../easy_c_8h.html',1,'']]],
+  ['err_2',['err',['../class_s_i114_x__light__sensor__easyc_1_1_easy_c.html#a693daef884a0a77504e298c01cf3a885',1,'SI114X_light_sensor_easyc::EasyC']]]
 ];
