f058174430b982a93b529f458fa21f4c5db41ef4
Docs Robot <robot@example.com>
2022-02-17 12:11:57 +0000

    Committing documentation changes

--- docs/hierarchy.js
+++ docs/hierarchy.js
@@ -1,6 +1,7 @@
 var hierarchy =
 [
-    [ "Digital_light_sensor_easyc::EasyC", "class_digital__light__sensor__easyc_1_1_easy_c.html", [
+    [ "Digital_light_sensor_easyc::EasyC", "class_digital__light__sensor__easyc_1_1_easy_c.html", null ],
+    [ "EasyC", null, [
       [ "LTR507", "class_l_t_r507.html", null ]
     ] ]
 ];
