f1f7b9a4c66fd6d7cd81f84f66397e37c3c9cd8e
Goran Jurić <74900926+gogi640@users.noreply.github.com>
2022-02-23 10:26:10 +0100

    Update ReadLightAndProximity.ino

--- examples/ReadLightAndProximity/ReadLightAndProximity/ReadLightAndProximity.ino
+++ examples/ReadLightAndProximity/ReadLightAndProximity/ReadLightAndProximity.ino
@@ -15,25 +15,28 @@ LTR507 sensor;
 
 void setup()
 {
-  Serial.begin(115200);
-  sensor.init();
+  Serial.begin(115200); //Start communication with PC via UART
+  sensor.init();        //Initialize sensor
 }
 
 void loop()
 {
-  if(sensor.Available()){
+  if(sensor.Available())  //Check if sensor is available
+  {
     uint16_t light; 
     light = sensor.getLightIntensity(); // get light intensity
     if(light!=0){
       Serial.print("Light intensity: ");
-      Serial.print(light);
+      Serial.print(light);              // print light intensity
       Serial.println(" lux.");
     }
     else{
-      Serial.println("Light intensity is not read or 0 lux.");
+      Serial.println("Light intensity is not read or 0 lux.");//If there was some kind of error
+                                                              //print message
     }
   }
-  else Serial.println("Communication error");
+  else Serial.println("Communication error"); // If sensor is not available,
+                                              // there was communication error
   Serial.println("");
   delay(1000);
 }
