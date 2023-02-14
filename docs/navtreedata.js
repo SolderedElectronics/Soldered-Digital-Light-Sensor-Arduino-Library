b2bb1e68400ba56c7c5182c2ccf9c968d8557225
Docs Robot <robot@example.com>
2023-02-14 15:29:18 +0000

    Committing documentation changes

--- docs/navtreedata.js
+++ docs/navtreedata.js
@@ -25,9 +25,7 @@
 var NAVTREE =
 [
   [ "Digital Light sensor Arduino library", "index.html", [
-    [ "Digital light & proximity sensor LTR-507 breakout Arduino library", "index.html", [
-      [ "Have fun!", "index.html#autotoc_md8", null ]
-    ] ],
+    [ "Digital light & proximity sensor LTR-507 breakout Arduino library", "index.html", "index" ],
     [ "Classes", "annotated.html", [
       [ "Class List", "annotated.html", "annotated_dup" ],
       [ "Class Index", "classes.html", null ],
